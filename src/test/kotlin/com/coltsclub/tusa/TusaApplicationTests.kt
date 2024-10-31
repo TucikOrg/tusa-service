17dbef7c258c082c0204792299d30cf66e40c1fc
artem <qzwsecrftb@gmail.com>
2024-10-31 11:34:30 +0300

    dev

--- src/test/kotlin/com/coltsclub/tusa/TusaApplicationTests.kt
+++ src/test/kotlin/com/coltsclub/tusa/TusaApplicationTests.kt
@@ -1,13 +1,7 @@
 package com.coltsclub.tusa
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class TusaApplicationTests {
+class TusaApplicationTests {}
 
-	@Test
-	fun contextLoads() {
-	}
-
-}
