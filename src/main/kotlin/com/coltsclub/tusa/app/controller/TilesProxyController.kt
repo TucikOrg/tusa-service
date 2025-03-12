612881cd5486915814e70dde35f25a5002c0ba67
artem <qzwsecrftb@gmail.com>
2025-03-12 11:25:39 +0300

    dev

--- src/main/kotlin/com/coltsclub/tusa/app/controller/TilesProxyController.kt
+++ src/main/kotlin/com/coltsclub/tusa/app/controller/TilesProxyController.kt
@@ -13,15 +13,27 @@ class TilesProxyController {
     @Value("\${app.mapbox.token}")
     private lateinit var token: String
 
+    private val cache: MutableMap<String, ByteArray> = mutableMapOf()
+
     @GetMapping("api/v1/tiles/{zoom}/{x}/{y}.mvt")
     fun getTile(
         @PathVariable zoom: Int,
         @PathVariable x: Int,
         @PathVariable y: Int
     ): ResponseEntity<ByteArray> {
+        // пробуем грузить из кэша
+        cache["$zoom/$x/$y"]?.let {
+            return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType("application/vnd.mapbox-vector-tile"))
+                .body(it)
+        }
+
         val urlStr = "https://api.mapbox.com/v4/mapbox.mapbox-streets-v8,mapbox.mapbox-terrain-v2/$zoom/$x/$y.mvt?access_token=$token"
         val url = URL(urlStr)
         val tileBytes = url.readBytes()
+
+        // сохраняем в кэш
+        cache["$zoom/$x/$y"] = tileBytes
 
         return ResponseEntity.ok()
             .contentType(MediaType.parseMediaType("application/vnd.mapbox-vector-tile"))
