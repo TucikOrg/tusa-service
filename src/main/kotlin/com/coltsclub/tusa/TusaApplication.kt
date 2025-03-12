72be4cfc9113359f94253f63a48f0f1c3c0ec137
artem <qzwsecrftb@gmail.com>
2025-03-12 10:38:22 +0300

    dev

--- src/main/kotlin/com/coltsclub/tusa/TusaApplication.kt
+++ src/main/kotlin/com/coltsclub/tusa/TusaApplication.kt
@@ -3,7 +3,7 @@ package com.coltsclub.tusa
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration
 import org.springframework.boot.runApplication
-///
+////
 
 @SpringBootApplication(exclude = [ErrorMvcAutoConfiguration::class])
 class TusaApplication
